e93b383a5a5a058cdb98ad1cd42a4316b1847af6
Tobias <1056318+tobias-gaenzler@users.noreply.github.com>
2021-02-02 11:46:32 +0100

    Fixed java version and package names.

--- src/main/java/de/tobiasgaenzler/javakatastart/FizzBuzz.java
+++ src/main/java/de/tobiasgaenzler/javakatastart/FizzBuzz.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package de.tobiasgaenzler.javakatastart;
 
 import java.util.stream.IntStream;
 
--- src/test/java/de/tobiasgaenzler/javakatastart/FizzBuzzTest.java
+++ src/test/java/de/tobiasgaenzler/javakatastart/FizzBuzzTest.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package de.tobiasgaenzler.javakatastart;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
