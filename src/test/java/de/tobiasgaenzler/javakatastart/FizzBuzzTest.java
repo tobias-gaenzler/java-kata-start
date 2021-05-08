35910ad35e70d4b3fd7309db05d35df5111262a5
tobias-gaenzler <tobias-gaenzler@users.noreply.github.com>
2021-05-08 20:44:15 +0200

    Correct test

--- src/test/java/de/tobiasgaenzler/javakatastart/FizzBuzzTest.java
+++ src/test/java/de/tobiasgaenzler/javakatastart/FizzBuzzTest.java
@@ -9,22 +9,22 @@ public class FizzBuzzTest {
 
     @ParameterizedTest
     @ValueSource(ints = {1, 2, 4, 7, 8, 11, 13, 14, 16, 17, 19, 22, 23, 26, 28, 29, 31, 32, 34, 37, 38, 41, 43, 44, 46, 47, 49, 52, 53, 56, 58, 59, 61, 62, 64, 67, 68, 71, 73, 74, 76, 77, 79, 82, 83, 86, 88, 89, 91, 92, 94, 97, 98})
-    public void shouldReturnNumberIfNotDivisibleBy3Or5() {
-        String result = new FizzBuzz().translateNumberToFizzBuzz(1);
-        assertThat(result).isEqualTo("1");
+    public void shouldReturnNumberIfNotDivisibleBy3Or5(Integer number) {
+        String result = new FizzBuzz().translateNumberToFizzBuzz(number);
+        assertThat(result).isEqualTo(number.toString());
     }
 
     @ParameterizedTest
     @ValueSource(ints = {3, 6, 9, 12, 18, 21, 24, 27, 33, 36, 39, 42, 48, 51, 54, 57, 63, 66, 69, 72, 78, 81, 84, 87, 93, 96, 99})
-    public void shouldReturnFizzForDivisibleBy3AndNot5() {
-        String result = new FizzBuzz().translateNumberToFizzBuzz(9);
+    public void shouldReturnFizzForDivisibleBy3AndNot5(Integer number) {
+        String result = new FizzBuzz().translateNumberToFizzBuzz(number);
         assertThat(result).isEqualTo("Fizz");
     }
 
     @ParameterizedTest
     @ValueSource(ints = {5, 10, 20, 25, 35, 40, 50, 55, 65, 70, 80, 85, 95, 100})
-    public void shouldReturnBuzzForDivisibleBy5AndNot3() {
-        String result = new FizzBuzz().translateNumberToFizzBuzz(10);
+    public void shouldReturnBuzzForDivisibleBy5AndNot3(Integer number) {
+        String result = new FizzBuzz().translateNumberToFizzBuzz(number);
         assertThat(result).isEqualTo("Buzz");
     }
 
